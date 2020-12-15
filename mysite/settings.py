5b5c733347645ac49144a8c149d30c2273ba40b7
DIKSITHK <71424829+DIKSITHK@users.noreply.github.com>
2020-12-16 00:41:19 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,7 +54,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
+        'APP_DIRS':
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
